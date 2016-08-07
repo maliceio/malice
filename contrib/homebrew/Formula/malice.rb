45ec29fdd2c2d7dfc3781add41ce123ac7733c08
blacktop <blacktop@users.noreply.github.com>
2016-08-07 12:44:20 -0600

    fix homebrew formula

--- contrib/homebrew/Formula/malice.rb
+++ contrib/homebrew/Formula/malice.rb
@@ -1,22 +1,20 @@
-require "language/go"
-
 class Malice < Formula
   desc "malice - VirusTotal Wanna Be - Now with 100% more Hipster"
   homepage "https://github.com/maliceio/malice"
   url "https://github.com/maliceio/malice.git",
     :revision => "d845ec0fa2788b26a36b7b5bf4440d0bf4502abb"
-
-  head "https://github.com/maliceio/malice.git", :shallow => false
+  version "0.1.0-alpha"
+  head "https://github.com/maliceio/malice.git"
 
   option "without-completions", "Disable bash/zsh completions"
 
   depends_on "go" => :build
   depends_on "libmagic" => :run
-  
+
   # It's possible that the user wants to manually install Docker and Machine,
   # for example, they want to compile Docker manually
-  depends_on "docker" => :recommended
-  depends_on "docker-machine" => :recommended
+  # depends_on "docker" => :recommended
+  # depends_on "docker-machine" => :recommended
 
   def install
     contents = Dir["{*,.git,.gitignore}"]
@@ -26,18 +24,18 @@ class Malice < Formula
     ENV["GOPATH"] = gopath
     ENV.prepend_create_path "PATH", gopath/"bin"
 
-    Language::Go.stage_deps resources, gopath/"src"
+    # Language::Go.stage_deps resources, gopath/"src"
 
-    (var/"log/vm-proxy-server").mkpath
+    (var/"log/malice").mkpath
 
     cd gopath/"src/github.com/maliceio/malice" do
       system "go", "get", "-v"
       system "go", "build", "-o", bin/"malice"
-      # bin.install "bin/vm-proxy-server"
+      # bin.install "bin/malice"
     end
 
     if build.with? "completions"
-      zsh_completion.install "contrib/completion/zsh/_docker"
+      zsh_completion.install gopath/"src/github.com/maliceio/malice/contrib/completion/zsh/_malice"
     end
   end
 

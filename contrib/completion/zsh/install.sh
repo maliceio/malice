8633380b1430f27da02777b3a4af85f4b8b26783
blacktop <blacktop@users.noreply.github.com>
2016-08-14 13:03:47 -0600

    add lookup to completions

--- contrib/completion/zsh/install.sh
+++ contrib/completion/zsh/install.sh
@@ -2,12 +2,16 @@
 set -e
 
 ZSH_FUNC_DIR="/usr/share/zsh/site-functions"
-# ZSH_FUNC_DIR="/usr/local/share/zsh/site-functions"
+ZSH_HOMEBREW_FUNC_DIR="/usr/local/share/zsh/site-functions"
 
-if [ -d "$ZSH_FUNC_DIR" ]; then
+if [ -d "$ZSH_HOMEBREW_FUNC_DIR" ]; then
+    echo "Installing into ${ZSH_HOMEBREW_FUNC_DIR}..."
+    sudo cp ./_malice "$ZSH_HOMEBREW_FUNC_DIR"
+    echo "Installed!"
+elif [ -d "$ZSH_FUNC_DIR" ]; then
     echo "Installing into ${ZSH_FUNC_DIR}..."
     sudo cp ./_malice "$ZSH_FUNC_DIR"
-    echo "Installed! Make sure that ${ZSH_FUNC_DIR} is in your \$fpath."
+    echo "Installed! Make sure that ${ZSH_FUNC_DIR} is in your \$fpath."    
 else
     echo "Could not find ${ZSH_FUNC_DIR}. Please install manually."
     exit 1

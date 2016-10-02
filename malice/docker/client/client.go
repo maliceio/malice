d3180a5fa34e34a1101f58e3396f8c80933e6495
blacktop <blacktop@users.noreply.github.com>
2016-10-02 13:28:30 -0600

    add debug statements

--- malice/docker/client/client.go
+++ malice/docker/client/client.go
@@ -9,6 +9,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/client"
+	"github.com/maliceio/go-plugin-utils/utils"
 	"github.com/maliceio/malice/config"
 )
 
@@ -31,34 +32,46 @@ func NewDockerClient() *Docker {
 	case "linux":
 		log.Debug("Running inside Docker...")
 		log.Debug("Creating NewClient...")
+		proto, addr, basePath, err := client.ParseHost("unix:///var/run/docker.sock")
+		log.Debug("Proto: ", proto, ", Addr: ", addr, ", BasePath: ", basePath, ", Error: ", err)
 		defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
-		docker, err = client.NewClient("unix:///var/run/docker.sock", "v1.23", nil, defaultHeaders)
-
+		docker, err = client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
+		ip = "localhost"
+		port = "2375"
 	case "darwin":
 		log.Debug("Running inside Docker for Mac...")
 		log.Debug("Creating NewClient...")
+		proto, addr, basePath, err := client.ParseHost("unix:///var/run/docker.sock")
+		log.Debug("Proto: ", proto, ", Addr: ", addr, ", BasePath: ", basePath, ", Error: ", err)
 		defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
-		docker, err = client.NewClient("unix:///var/run/docker.sock", "v1.23", nil, defaultHeaders)
+		docker, err = client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
+		ip = "localhost"
+		port = "2375"
 	case "windows":
 		log.Debug("Creating NewEnvClient...")
 		docker, err = client.NewEnvClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		ip, port, err = parseDockerEndoint(utils.Getopt("DOCKER_HOST", config.Conf.Docker.EndPoint))
 	default:
 		log.Debug("Creating NewEnvClient...")
 		docker, err = client.NewEnvClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		ip, port, err = parseDockerEndoint(utils.Getopt("DOCKER_HOST", config.Conf.Docker.EndPoint))
 	}
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// Check if client can connect
 	log.Debug("Docker Info...")
 	if _, err = docker.Info(context.Background()); err != nil {
 		log.Debug("Docker Info FAILED...")
 		handleClientError(err)
 	} else {
-		ip = "localhost"
-		port = "2375"
-		log.WithFields(log.Fields{"ip": ip, "port": port}).Debug("Connected to docker daemon native client")
+		log.WithFields(log.Fields{"ip": ip, "port": port}).Debug("Connected to docker daemon client")
 	}
 
 	return &Docker{

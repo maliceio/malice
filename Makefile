d18ab2e77ce1cc4685f6cb364d97ddc53b52474d
blacktop <blacktop@users.noreply.github.com>
2016-10-08 21:12:11 -0600

    almost done with project:7

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME=malice
 ARCH=$(shell uname -m)
-VERSION=0.1.0-alpha
+VERSION=0.2.0-alpha
 
 all: deps test validate
 
@@ -15,11 +15,7 @@ docker:
 	docker build -t malice/build-binaries -f Dockerfile.binaries .
 
 build: bindata docker
-	# mkdir -p build/Linux  && GOOS=linux  go build -ldflags "-X main.Version=$(VERSION)" -o build/Linux/$(NAME)
-	rm -rf build
-	mkdir build
 	docker run --rm -v `pwd`:/go/src/github.com/maliceio/malice:rw malice/build-binaries
-	# mkdir -p build/Darwin && GOOS=darwin go build -ldflags "-X main.Version=$(VERSION)" -o build/Darwin/$(NAME)
 
 deps:
 	go get -u github.com/progrium/gh-release/...
@@ -44,8 +40,9 @@ lint:
 
 release: build
 	rm -rf release && mkdir release
-	tar -zcf release/$(NAME)_$(VERSION)_linux_$(ARCH).tgz -C build/Linux $(NAME)
-	tar -zcf release/$(NAME)_$(VERSION)_darwin_$(ARCH).tgz -C build/Darwin $(NAME)
+	mv build/*.zip release/
+	# tar -zcf release/$(NAME)_$(VERSION)_linux_$(ARCH).tgz -C build/Linux $(NAME)
+	# tar -zcf release/$(NAME)_$(VERSION)_darwin_$(ARCH).tgz -C build/Darwin $(NAME)
 	gh-release create maliceio/$(NAME) $(VERSION) $(shell git rev-parse --abbrev-ref HEAD) v$(VERSION)
 
 destroy:
--- install/scripts/compile.sh
+++ install/scripts/compile.sh
@@ -12,9 +12,9 @@ go get -v -d ./...
 
 # Delete the old dir
 echo "==> Removing old directory..."
-rm -f bin/*
-rm -rf pkg/*
-mkdir -p bin/
+rm -rf build/*
+rm -rf release/*
+mkdir -p release
 
 # Get the git commit
 GIT_COMMIT=$(git rev-parse HEAD)
@@ -22,20 +22,30 @@ GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
 GIT_DESCRIBE=$(git describe --tags)
 
 # Determine the arch/os combos we're building for
-XC_ARCH=${XC_ARCH:-"386 amd64"}
+XC_ARCH=${XC_ARCH:-"arm amd64"}
 XC_OS=${XC_OS:-"solaris darwin freebsd linux"}
 
 # Build!
-echo "==> Building..."
-for GOOS in darwin linux; do
-  for GOARCH in 386 amd64; do
-    go build -v -ldflags "-X main.GitCommit=${GIT_COMMIT}${GIT_DIRTY} -X main.GitDescribe=${GIT_DESCRIBE}" -o build/$GOOS_$GOARCH/malice
-  done
+echo "==> Building linux..."
+for GOARCH in arm amd64; do
+go build -v -ldflags "-X main.GitCommit=${GIT_COMMIT}${GIT_DIRTY} -X main.GitDescribe=${GIT_DESCRIBE}" -o build/linux_${GOARCH}/malice
 done
-# gox \
-#     -os="${XC_OS}" \
-#     -arch="${XC_ARCH}" \
-#     -ldflags "-X main.GitCommit=${GIT_COMMIT}${GIT_DIRTY} -X main.GitDescribe=${GIT_DESCRIBE}" \
-#     -output "build/{{.OS}}_{{.Arch}}/malice" \
-#     .
 
+echo "==> Building darwin..."
+go build -v -ldflags "-X main.GitCommit=${GIT_COMMIT}${GIT_DIRTY} -X main.GitDescribe=${GIT_DESCRIBE}" -o build/darwin_amd64/malice
+
+# Zip and copy to the dist dir
+echo "==> Packaging..."
+for PLATFORM in $(find ./build -mindepth 1 -maxdepth 1 -type d); do
+    OSARCH=$(basename ${PLATFORM})
+    echo "--> ${OSARCH}"
+
+    pushd $PLATFORM >/dev/null 2>&1
+    zip ../${OSARCH}.zip ./*
+    popd >/dev/null 2>&1
+done
+
+# Done!
+echo
+echo "==> Results:"
+ls -hl build/

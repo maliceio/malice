964440d72126217b145f8b6a95acf4a0a5bc9c0e
blacktop <blacktop@users.noreply.github.com>
2017-04-21 16:15:25 -0600

    update config stuff

--- config/load.go
+++ config/load.go
@@ -12,84 +12,81 @@ import (
 	"github.com/maliceio/malice/malice/maldirs"
 )
 
-// "github.com/pelletier/go-toml"
-
 // Configuration represents the malice runtime configuration.
 type Configuration struct {
-	Title       string
-	Version     string
-	Author      authorInfo
-	Web         webConfig
-	Email       emailConfig
-	DB          databaseConfig      `toml:"database"`
-	UI          userInterfaceConfig `toml:ui`
-	Environment envConfig
-	Docker      dockerConfig
-	Logger      loggerConfig
-	Proxy       proxyConfig
+	Title       string              `toml:"title" json:"title,omitempty"`
+	Version     string              `toml:"version" json:"version,omitempty"`
+	Author      authorInfo          `toml:"author" json:"author,omitempty"`
+	Web         webConfig           `toml:"web" json:"web,omitempty"`
+	Email       emailConfig         `toml:"email" json:"email,omitempty"`
+	DB          databaseConfig      `toml:"database" json:"db,omitempty"`
+	UI          userInterfaceConfig `toml:"ui" json:"ui,omitempty"`
+	Environment envConfig           `toml:"environment" json:"environment,omitempty"`
+	Docker      dockerConfig        `toml:"docker" json:"docker,omitempty"`
+	Logger      loggerConfig        `toml:"logger" json:"logger,omitempty"`
+	Proxy       proxyConfig         `toml:"proxy" json:"proxy,omitempty"`
 }
 
 type authorInfo struct {
-	Name         string
-	Organization string
-	Email        string
+	Name         string `json:"name,omitempty"`
+	Organization string `json:"organization,omitempty"`
 }
 
 type webConfig struct {
-	URL      string
-	AdminURL string `toml:"admin_url"`
+	URL      string `json:"url,omitempty"`
+	AdminURL string `toml:"admin_url" json:"admin_url,omitempty"`
 }
 
 type userInterfaceConfig struct {
-	Name    string
-	Image   string
-	Server  string
-	Ports   []int
-	Enabled bool
+	Name    string `json:"name,omitempty"`
+	Image   string `json:"image,omitempty"`
+	Server  string `json:"server,omitempty"`
+	Ports   []int  `json:"ports,omitempty"`
+	Enabled bool   `json:"enabled,omitempty"`
 }
 
 type databaseConfig struct {
-	Name    string
-	Image   string
-	Server  string
-	Ports   []int
-	Timeout int
-	Enabled bool
+	Name    string `json:"name,omitempty"`
+	Image   string `json:"image,omitempty"`
+	Server  string `json:"server,omitempty"`
+	Ports   []int  `json:"ports,omitempty"`
+	Timeout int    `json:"timeout,omitempty"`
+	Enabled bool   `json:"enabled,omitempty"`
 }
 
 type emailConfig struct {
-	Host     string
-	port     int
-	Username string `toml:"user"`
-	Password string `toml:"pass"`
+	Host     string `json:"host,omitempty"`
+	port     int    `json:"port,omitempty"`
+	Username string `toml:"user" json:"username,omitempty"`
+	Password string `toml:"pass" json:"password,omitempty"`
 }
 
 type envConfig struct {
-	Run string
+	Run string `json:"run,omitempty"`
 }
 
 type dockerConfig struct {
-	Name     string `toml:"machine-name"`
-	EndPoint string
-	Timeout  time.Duration
-	Binds    string
-	Links    string
-	CPU      int64
-	Memory   int64
+	Name     string        `toml:"machine-name" json:"name,omitempty"`
+	EndPoint string        `json:"endpoint,omitempty"`
+	Timeout  time.Duration `json:"timeout,omitempty"`
+	Binds    string        `json:"binds,omitempty"`
+	Links    string        `json:"links,omitempty"`
+	CPU      int64         `json:"cpu,omitempty"`
+	Memory   int64         `json:"memory,omitempty"`
 }
 
 type loggerConfig struct {
-	FileName   string
-	MaxSize    int
-	MaxAge     int
-	MaxBackups int
-	LocalTime  bool
+	FileName   string `json:"filename,omitempty"`
+	MaxSize    int    `json:"maxsize,omitempty"`
+	MaxAge     int    `json:"maxage,omitempty"`
+	MaxBackups int    `json:"maxbackups,omitempty"`
+	LocalTime  bool   `json:"localtime,omitempty"`
 }
 
 type proxyConfig struct {
-	Enable bool
-	HTTP   string
-	HTTPS  string
+	Enable bool   `json:"enable,omitempty"`
+	HTTP   string `json:"http,omitempty"`
+	HTTPS  string `json:"https,omitempty"`
 }
 
 // Conf represents the Malice runtime configuration

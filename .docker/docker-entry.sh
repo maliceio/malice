ad9f99f2d60613b9a2805ec4054e406b1afb01e8
blacktop <blacktop@users.noreply.github.com>
2023-04-03 17:01:13 -0600

    update copyright date

--- .docker/docker-entry.sh
+++ .docker/docker-entry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Copyright (c) 2013 - 2015 blacktop Joshua Maine, All Rights Reserved.
+# Copyright (c) 2013 - 2023 blacktop, All Rights Reserved.
 # See LICENSE for license information.
